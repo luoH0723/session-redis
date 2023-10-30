799800e0afaba00bbc13fa2af1cc2d74aa8a40c5
lh <3035419253@qq.com>
2023-10-30 17:51:45 +0800

    修改测试

--- src/main/java/com/lh/sessionredis/SessionRedisApplication.java
+++ src/main/java/com/lh/sessionredis/SessionRedisApplication.java
@@ -2,8 +2,10 @@ package com.lh.sessionredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+//@EnableRedisHttpSession
 public class SessionRedisApplication {
 
     public static void main(String[] args) {
